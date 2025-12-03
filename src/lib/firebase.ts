0aeb8bd3300240b85e0ad0e110874dee39a2be4d
Shaswata-codes <shaswatasarkar05@gmail.com>
2025-12-03 21:42:25 +0530

    environment vars changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAvJ1NDdYf8KmXH0MDT46vA0ocrMTyMZY8",
-  authDomain: "compiler-1ff17.firebaseapp.com",
-  projectId: "compiler-1ff17",
-  storageBucket: "compiler-1ff17.firebasestorage.app",
-  messagingSenderId: "1063380182244",
-  appId: "1:1063380182244:web:89dc8f8b036cb4dca338f1"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
@@ -16,3 +16,4 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication
 export const auth = getAuth(app);
 export default app;
+
